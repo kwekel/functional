bb1ad1b3b8f6b3efd03a95992caabce340757fe2
unknown <stefanhabets92@gmail.com>
2024-03-17 11:47:43 +0100

    composition

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import { RecursiveComponent } from "./components/recursive";
+import { GreenSmallButton, RedButton } from './components/composition';
+import { RecursiveComponent } from './components/recursive';
 
 const myNestedObject = {
-  key1: "value1",
+  key1: 'value1',
   key2: {
-    innerKey1: "innerValue1",
+    innerKey1: 'innerValue1',
     innerKey2: {
-      innerInnerKey1: "innerInnerValue1",
-      innerInnerKey2: "innerInnerValue2",
+      innerInnerKey1: 'innerInnerValue1',
+      innerInnerKey2: 'innerInnerValue2',
     },
   },
-  key3: "value3",
+  key3: 'value3',
 };
 
 function App() {
   return (
     <>
       <RecursiveComponent data={myNestedObject} />
+      <RedButton text="I am red" />
+      <GreenSmallButton text="I small and green" />
     </>
   );
 }
